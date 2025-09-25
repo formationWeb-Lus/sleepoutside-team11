a7b10d514af9dcea27e50368d08aa5cae1288902
alexandre lusa <jiresselusa127@gmail.com>
2025-09-25 07:18:42 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   root: "src",
   build: {
-    outDir: "dist", // plus de ../
+    outDir: "dist", 
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
