386d1a191b17361aae7d25ae9c1780c23361be40
alexandre lusa <jiresselusa127@gmail.com>
2025-09-28 20:43:26 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,15 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: "src", // dossier source de Vite
+  root: "src",
   build: {
-    outDir: "../dist", // build final à la racine dans dist
+    outDir: "../dist",
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        // Supprime ou commente les lignes si les fichiers n'existent pas
-         cart: resolve(__dirname, "src/cart/index.html"),
-         checkout: resolve(__dirname, "src/checkout/index.html"),
-         product_listing: resolve(__dirname, "src/product_listing/index.html"),
+        cart: resolve(__dirname, "cart/index.html"),
+        checkout: resolve(__dirname, "checkout/index.html"),
+        product_listing: resolve(__dirname, "product_listing/index.html"),
       },
     },
   },
